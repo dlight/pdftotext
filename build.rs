a5e5a8d291c52ca9ebf8296f438a8b7d120fb24b
Elias Gabriel Amaral da Silva <tolkiendili@gmail.com>
2020-12-16 06:39:06 -0300

    Maybe it will work this time?

--- build.rs
+++ build.rs
@@ -1,23 +1,25 @@
 fn main() {
     if let Ok(_) = std::env::var("DOCS_RS") {
-        let poppler = pkg_config::Config::new()
-        .cargo_metadata(true)
-        .probe("poppler")
-        .expect("pkg-config could not find poppler");
-
-        let mut build = cc::Build::new();
-
-        let mut callpoppler = build
-            .cpp(true)
-            .file("src/callpoppler.cc");
-        
-        for dir in &poppler.include_paths {
-            callpoppler = callpoppler.include(dir);
-        }
-
-        callpoppler.compile("callpoppler.a");
-
-        //shouldn't cc take care of this?
-        println!("cargo:rerun-if-changed=src/callpoppler.cc");
+        return;
     }
+    
+    let poppler = pkg_config::Config::new()
+    .cargo_metadata(true)
+    .probe("poppler")
+    .expect("pkg-config could not find poppler");
+
+    let mut build = cc::Build::new();
+
+    let mut callpoppler = build
+        .cpp(true)
+        .file("src/callpoppler.cc");
+        
+    for dir in &poppler.include_paths {
+        callpoppler = callpoppler.include(dir);
+    }
+
+    callpoppler.compile("callpoppler.a");
+
+    //shouldn't cc take care of this?
+    println!("cargo:rerun-if-changed=src/callpoppler.cc");
 }
